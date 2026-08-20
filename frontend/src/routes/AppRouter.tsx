e5bc80e987474a26b523c6bcfc193e6ffb2bfeb3
solia142 <thf5465@gmail.com>
2026-08-20 15:00:06 +0900

    ✨ feat: 회원가입·로그인 Frontend 구현 (#29)
    * ✨ feat: 회원가입 로그인 Frontend 구현
    
    * 💄 style: 회원가입 입력 형식 안내 추가
    
    * 🎨 style: 로그인 토큰 저장 코드 들여쓰기 정리
    
    * 🐛 fix: 인증 요청에 credentials 설정 추가

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
+import LoginPage from '../pages/LoginPage'
+import SignupPage from '../pages/SignupPage'
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
